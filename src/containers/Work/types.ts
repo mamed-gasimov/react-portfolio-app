fe40062c9a7a87ca4d12d045c1d351a9f7a95bb8
Mammadali Gasimov <mamed-gasimov@Mamed.local>
2022-03-15 22:52:09 +0400

    added work container and its styles, and types

---
+++ src/containers/Work/types.ts
@@ -0,0 +1,9 @@
+export type WorkType = {
+    name: string;
+    projectLink: string;
+    codeLink: string;
+    imgUrl: string;
+    title: string;
+    description: string;
+    tags: string[];
+}
